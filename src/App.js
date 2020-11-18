50211ed9acfadb27d1bbb1a4ff57d62df66adbd6
KVlearn <kavya.mdu@gmail.com>
2020-11-18 12:58:39 -0800

    completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
-
+//ProductContext and CartContext Object from context
+import {ProductContext} from './contexts/ProductContext';
+import {CartContext} from './contexts/CartContext';
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -13,21 +15,38 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		//receives the product - just add to cart
+		console.log('item in app',item) //is an object
+		console.log('cart=',cart)//is an array
+		setCart([...cart,item])
 	};
 
+	const removeItem=(inputItemId)=>{
+		console.log('!!!im in removeItem!!!')
+		console.log('itemid=',inputItemId)
+		console.log('cart=',cart)
+		setCart(cart.filter(item=> item.id !== inputItemId))
+	}
+
 	return (
+		//Step#1 wrap app with imported ProductContext so data is distributed
+		//Step#2 pass the value as props to the provider
+		<ProductContext.Provider value={{products,addItem}}>
+		<CartContext.Provider value={{cart,removeItem}}>
 		<div className="App">
-			<Navigation cart={cart} />
+			<Navigation />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products  />
 			</Route>
 
 			<Route path="/cart">
-				<ShoppingCart cart={cart} />
+				<ShoppingCart />
 			</Route>
 		</div>
+		</CartContext.Provider>
+		</ProductContext.Provider>
 	);
 }
 
---
+++ src/contexts/CartContext.js
@@ -0,0 +1,5 @@
+//import createContext
+import {createContext} from 'react';
+
+//create CartContext object
+export const CartContext = createContext();
